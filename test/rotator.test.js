5c88636c614d1f8d1312493b599860796ab6cd1d
Jared Hanson <jaredhanson@gmail.com>
2014-09-18 14:08:35 -0700

    Rotator: emit events, hide private ivars, etc.

--- test/rotator.test.js
+++ test/rotator.test.js
@@ -12,7 +12,7 @@ describe('sks/rotator', function() {
     var store = new pkg.MemoryKeyStore();
     var config = {};
     var generator = function(cb){cb('no');};
-    var rotator = new pkg.Rotator(store,config,generator);
+    var rotator = new pkg.Rotator(store,generator,config);
     expect(rotator).to.be.an('object');
     expect(rotator.start).to.be.a('function');
     expect(rotator.frequency).to.be.equal(86400*1000);
@@ -22,7 +22,7 @@ describe('sks/rotator', function() {
     var store = new pkg.MemoryKeyStore();
     var config = {};
     var generator = function(cb){cb(false, 'test', 'key', 'cert');};
-    var rotator = new pkg.Rotator(store,config,generator);
+    var rotator = new pkg.Rotator(store,generator,config);
     rotator.rotate(function(err){
       expect(err).to.not.exist;
       expect(store._keys.length).to.be.equal(1);
